9e824c9c895a3b953f20111723f00598d9321e1a
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2024-05-14 21:58:57 -0300

    Atividade de Java

---
+++ Atividade/Shape.java
@@ -0,0 +1,40 @@
+package Segundaatividade;
+
+public abstract class Shape {
+    public abstract String getName();
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+    // public String nome;
+    // public double comprimento;
+    // public double largura;
+    // public double area;
+
+    // public double getArea() {
+    //     return area;
+    // }
+
+    // public void setArea(double area) {
+    //     this.area = area;
+    // }
+
+    // public void setComprimento(double comprimento) {
+    //     this.comprimento = comprimento;
+    // }
+
+    // public double getLargura() {
+    //     return largura;
+    // }
+
+    // public void setLargura(double largura) {
+    //     this.largura = largura;
+    // }
+
+    // public String getNome() {
+    //     return nome;
+    // }
+
+    // public void setNome(String nome) {
+    //     this.nome = nome;
+    // }
+
+}
